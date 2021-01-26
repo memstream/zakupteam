5be7f50583e21dd8b44e66f746b1cf36fdbf3745
memstream <memstream@users.noreply.github.com>
2021-01-26 15:29:15 +0300

    Иконка ближайших торгов показывается только если она есть

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php require_once __DIR__ . '/php/p/begin_app.php'; ?>
 <div id="content">
-	<div class="title-with-icon">
-		<img src="image/trade.png" class="title-icon">
-		Ближайшие торги
-	</div>
-	<ul>
-		<?php foreach(R::findAll('favorite') as $f): ?>
-			<?php foreach(R::findAll('tender', ' tradedate >= ? AND id = ? AND tradedate IS NOT NULL ORDER BY tradedate ', [ date('Y-m-d H:i:s'), $f->tender_id ]) as $tender):?>
+	<?php 
+	$favorites = R::findAll('favorite');
+	$tenders = array();
+	foreach($favorites as $f) {
+		$tenders = array_merge($tenders, R::findAll(
+			'tender', ' tradedate >= ? AND id = ? AND tradedate IS NOT NULL ORDER BY tradedate ', 
+			[ date('Y-m-d H:i:s'), $f->tender_id ]
+		));
+	}
+	if(count($tenders)):
+	?>
+		<div class="title-with-icon">
+			<img src="image/trade.png" class="title-icon">
+			Ближайшие торги
+		</div>
+		<ul>
+			<?php foreach($tenders as $tender): ?>
 				<li>[<?= substr($tender->tradedate, 0, -9) ?>] <a href="favorite.php#<?= _XSS($tender->n) ?>"><?= _XSS($tender->info) ?></a></li>
 			<?php endforeach; ?>
-		<?php endforeach; ?>
-	</ul>
+		</ul>
+	<?php endif; ?>
 </div>
 <?php require_once __DIR__ . '/php/p/end.php'; ?>
