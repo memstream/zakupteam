538e9a57375e4157bec7037d0f326dfa72ddc18f
memstream <memstream@users.noreply.github.com>
2021-02-20 09:09:40 +0300

    Исправил сортировку

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php require_once __DIR__ . '/php/p/begin_app.php'; ?>
 <div id="content">
-	<?php 
-	$favorites = R::findAll('favorite');
-	$tenders = array();
-	foreach($favorites as $f) {
-		$tenders = array_merge($tenders, R::findAll(
-			'tender', ' tradedate >= ? AND id = ? AND tradedate IS NOT NULL ORDER BY tradedate ', 
-			[ date('Y-m-d H:i:s'), $f->tender_id ]
-		));
-	}
+	<?php
+	$tenders = R::findAll('tender', ' JOIN favorite ON tender.id = favorite.tender_id AND tender.tradedate IS NOT NULL AND tender.tradedate >= ? ORDER BY tender.tradedate ',
+		[ date('Y-m-d H:i:s') ]
+	);
 	if(count($tenders)):
 	?>
 		<div class="title-with-icon">
